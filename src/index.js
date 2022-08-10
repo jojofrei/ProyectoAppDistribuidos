0f41c447de45712f0588196a44ffaa630c47b7fe
jojofrei <jojofrei@espol.edu.ec>
2022-08-09 19:31:17 -0500

    aplicacion web

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const {engine} = require('express-handlebars');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MySQLStore = require('express-mysql-session');
+
+const { database } = require('./keys');
+
+// Intializations
+const app = express();
+require('./lib/passport');
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(session({
+  secret: 'appdistribuidos',
+  resave: false,
+  saveUninitialized: false,
+  store: new MySQLStore(database)
+}));
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Global variables
+app.use((req, res, next) => {
+  app.locals.success = req.flash('success');
+  app.locals.message = req.flash('message');
+  next();
+});
+
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Starting
+app.listen(app.get('port'), () => {
+  console.log('Server is in port', app.get('port'));
+});
