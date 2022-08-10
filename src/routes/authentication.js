bea1b3cf433664e1ebf3a667ed7a9238897d74a3
jojofrei <jojofrei@espol.edu.ec>
2022-08-09 21:22:06 -0500

    front end

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,22 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLoggedIn, (req, res) => {
     res.render('auth/signup');
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup',
     failureFlash: true
 }));
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isNotLoggedIn, (req, res) => {
     res.render('auth/signin');
 });
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local.signin', {
         successRedirect: '/profile',
         failureRedirect: '/signin',
@@ -26,8 +27,15 @@ router.post('/signin', (req, res, next) => {
 });
 
 
-router.get('/profile', (req, res) => {
-    res.send('Este es tu perfil');
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile');
+});
+
+router.get('/logout', isLoggedIn, (req, res, next) => {
+    req.logOut(req.user, err=> {
+        if(err) return next(err);
+        res.redirect('/signin');
+    });
 });
 
 module.exports = router;
